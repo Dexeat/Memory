535558aea662039006506d31cec34ba7866197d4
Dexeat <Dexeat@users.noreply.github.com>
2020-05-13 15:03:12 +0200

    v0.0

--- game.js
+++ game.js
@@ -1,24 +1,35 @@
 var config = {
+
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'phaser-example',
+    width: 800,
+    height: 600,
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
-        },
-
+            debug: true
+        }
     },
     scene: {
+        init: init,
         preload: preload,
         create: create,
         update: update
-    },
-};
+    }
 
-function preload(){}
+}
 
-function create(){}
+var game = new Phaser.Game(config);
+
+function init() {
+    var bat;
+}
+
+function preload(){
+    this.load.image('bg','Assets/background.jpg')
+}
+
+function create(){
+    fond = this.add.image(300,400,'bg')//.setOrigin(0);
+}
 
 function update(){}
